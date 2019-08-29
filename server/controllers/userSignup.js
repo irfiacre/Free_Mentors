e54d2e258dc17fc33ecc9897a2d264256106be83
irfiacre <firaduka@outlook.com>
2019-08-29 23:00:48 +0200

    feature (Signup Endpoint): adding Signup Endpoint - Adding user-signup controllers - Adding signup validator in middleware - Modifying userRoute - Modifying server.js
    [finishes #168200904]

---
+++ server/controllers/userSignup.js
@@ -0,0 +1,56 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const users= require('../models/users');
+
+const signup = async(req,res)=>{
+    const user = users.find(objectof=>objectof.email === req.body.email);
+    if(user){
+        return res.status(409).json({
+            status:409,
+            error:"Email already exist"
+        })
+    }
+    const newUser={
+        id:users.length+1,
+        firstName : req.body.firstName ,
+        lastName : req.body.lastName ,
+        email :  req.body.email,
+        password :  await bcrypt.hash(req.body.password,10),
+        address :  req.body.address,
+        bio :  req.body.bio,
+        occupation :  req.body.occupation,
+        expertise :  req.body.expertise,
+        is_mentor: req.body.is_mentor || false,
+        is_admin: req.body.is_admin || false
+
+    }
+    
+
+    const token = jwt.sign({
+        id: newUser.id,
+        fullname:(newUser.firstName + newUser.lastName),
+        email : newUser.email,
+        is_mentor: newUser.is_mentor,
+        is_admin: newUser.is_admin
+    },'jwtprivatekey');
+   
+    
+
+    users.push(newUser);
+
+    res.status(201).json({
+        status:201,
+        message:"user successfuly created",
+        data:{
+            token,
+            message:"user successfuly created"
+        }
+    })
+}
+
+
+module.exports = signup;
+
+
+
+
---
+++ server/middleware/signup.js
@@ -0,0 +1,26 @@
+
+import Joi from 'joi';
+
+const signupValidator = (req,res,next)=>{
+    const schema = {
+        firstName: Joi.string().alphanum().min(3).max(30).required(),
+        lastName: Joi.string().alphanum().min(3).max(30).required(),
+        password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
+        email: Joi.string().email({ minDomainSegments: 2 }),
+        address :  Joi.string().min(3).max(30).required(),
+        bio :  Joi.string().min(0).max(300).required(),
+        occupation :  Joi.string().min(3).max(30).required(),
+        expertise :  Joi.string().min(3).max(30).required(),
+     
+    }
+    const {error} = Joi.validate(req.body,schema);
+    if (error){
+        return res.status(400).json({
+            status:400,
+            error: error.details[0].message
+        });
+    }
+    next();
+}
+
+module.exports = signupValidator;
