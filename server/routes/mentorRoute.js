6134293df54541b89d4ed3ae1cde4e54c44355e0
irfiacre <firaduka@outlook.com>
2019-09-05 14:56:35 +0200

    #168313506 Removing spaces
     - Deleting comments.
    
     [finishes #168313506]

--- server/routes/mentorRoute.js
+++ server/routes/mentorRoute.js
@@ -5,7 +5,6 @@ import  accept from "../controllers/accept";
 import  reject from "../controllers/reject";
 import mentorCheck from '../middleware/mentorCheck';
 const route = express.Router();
-
 route.patch('/api/v1/sessions/:sessionId/accept',[Authentication,mentorCheck],accept);
 route.patch('/api/v1/sessions/:sessionId/reject',[Authentication,mentorCheck],reject);
 
