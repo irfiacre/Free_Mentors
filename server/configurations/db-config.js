7cc71917aba4f55398e9514d0dbcb367dba6a0d1
irfiacre <firaduka@outlook.com>
2019-09-08 20:40:25 +0200

    #168363093 database setup  - adding a new folder,configurations  - adding a new file db-config  - modifying package.json, package-lock.json
     [finishes #168363093]

---
+++ server/configurations/db-config.js
@@ -0,0 +1,33 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+
+
+dotenv.config();
+
+let pool;
+
+if (process.env.NODE_ENV === 'development') {
+  pool = new Pool({
+    connectionString: process.env.DEVELOPMENT_URL,
+  });
+
+  pool.on('connect', () => {
+  });
+} else if (process.env.NODE_ENV === 'testing') {
+  pool = new Pool({
+    connectionString: process.env.TEST_URL,
+  });
+
+  pool.on('connect', () => {
+  });
+} else if (process.env.NODE_ENV === 'production') {
+  pool = new Pool({
+    connectionString: process.env.PRODUCTION_URL,
+  });
+
+  pool.on('connect', () => {
+  });
+}
+
+export default pool;
