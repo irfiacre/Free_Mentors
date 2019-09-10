2ff61bc590e2a237053b9d92dfc91944b635a6e8
irfiacre <firaduka@outlook.com>
2019-09-10 18:23:40 +0200

    adding  modified files  and userRoute  [finishes #168367911]

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,12 +1,15 @@
-
-import express from 'express' ;
-import Authorisation from '../middleware/Authentication';
-
+import express  from'express';
+import signinValidater  from'../middleware/signin';
+import signin  from'../controllers/userSignin';
+import signupValidater  from'../middleware/signup'; 
+import signup  from'../controllers/userSignup';
+import Authorisation  from'../middleware/Authentication';
 import allMentors from '../controllers/allMentors';
 
 const route = express.Router();
 
-route.get('/api/v1/mentors', Authorisation, allMentors.MentorsDisplay);
-
+route.post('/api/v1/auth/signin',signinValidater,signin);
+route.post('/api/v1/auth/signup',signupValidater,signup);
+route.get('/api/v1/mentors',Authorisation,allMentors.MentorsDisplay);
 
 export default route;
