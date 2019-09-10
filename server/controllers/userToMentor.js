39389fd0f3673982f1ae9a6dc0301d490aeedba7
irfiacre <firaduka@outlook.com>
2019-09-10 19:34:43 +0200

    adding  modified file userToMentor  [finishes #168367891]

--- server/controllers/userToMentor.js
+++ server/controllers/userToMentor.js
@@ -3,31 +3,31 @@ import pool from '../configurations/db-config';
 
 const changeUserToMentor = async(req,res)=>{
     try{
-        const mentors ='SELECT * FROM USERS'
-        const {rows} = await pool.query(mentors)
-        
-    const checking = rows.find((objectof) => objectof.id === parseInt(req.params.userId));
-     if(!checking){
+        if(isNaN(req.params.userId)){
+            return res.status(400).json({
+                status: 400,
+                error: "The userId must be an integer"
+            })
+        }
+
+        const mentors =`SELECT * FROM USERS WHERE id = ${req.params.userId}`
+        const {rows:[checking]} = await pool.query(mentors) 
+       
+        if(!checking){
          return res.status(404).json({
              status:404,
              message:"user is not found"
          })
      }
-     console.log(checking);
-     
+    
      if(checking.is_mentor === true){
-         return res.status(409).json({
-             status: 409,
+         return res.status(422).json({
+             status: 422,
              message:"User is arleady mentor"
          })
         }
-     
-     const objIndex= rows.findIndex((obj) => obj.id === parseInt(req.params.userId));
-     rows[objIndex].is_mentor = true;  
-
      const newInfo= `UPDATE users SET is_mentor=true WHERE id = ${req.params.userId} `
      const {row} = await pool.query(newInfo)
-     console.log(row);
 
      res.status(200).json({
          status:200,
