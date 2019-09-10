388a0521e100d5dcdaf5feb72107eb3f9a30c5e4
irfiacre <firaduka@outlook.com>
2019-09-10 18:07:05 +0200

    adding  modified files  and userRoute  [finishes #168367911]

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,26 +1,9 @@
 import express from 'express' ;
-import signinValidater from '../middleware/signin';
-import signin from '../controllers/userSignin';
-import signupValidater from '../middleware/signup';
-import signup from '../controllers/userSignup';
 import Authorisation from '../middleware/Authentication';
 import allMentors from '../controllers/allMentors';
-import specificMentor from '../controllers/specificMentor';
-import sessionValidator from '../middleware/sessionValidator';
-import session from '../controllers/session';
-import userSessions from '../controllers/allsessions';
-import userReview from '../controllers/review';
-import reviewValidater from '../middleware/reviewValidater';
 
 const route = express.Router();
 
-route.post('/api/v1/auth/signin', signinValidater, signin);
-route.post('/api/v1/auth/signup', signupValidater, signup);
 route.get('/api/v1/mentors', Authorisation, allMentors.MentorsDisplay);
-route.get('/api/v1/mentors/:mentorId', Authorisation, specificMentor);
-route.post('/api/v1/sessions', [Authorisation, sessionValidator], session);
-route.get('/api/v1/sessions', Authorisation, userSessions);
-route.post('/api/v1/sessions/:sessionId/review', [Authorisation, reviewValidater], userReview);
-
 
 export default route;
