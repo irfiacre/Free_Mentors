0bf6f468c0a8d4599ecbae95db2eeab93230b797
irfiacre <firaduka@outlook.com>
2019-09-10 21:35:46 +0200

    adding  modified file specificMentor and userRoute  [finishes #168367925]

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -12,7 +12,7 @@ const route = express.Router();
 
 route.post('/api/v1/auth/signin',signinValidater,signin);
 route.post('/api/v1/auth/signup',signupValidater,signup);
-route.get('/api/v1/mentors',Authorisation,allMentors);
+route.get('/api/v1/mentors',Authorisation,allMentors.MentorsDisplay);
 route.get('/api/v1/mentors/:mentorId',Authorisation, specificMentor.MentorDisplay);
 
 export default route;
