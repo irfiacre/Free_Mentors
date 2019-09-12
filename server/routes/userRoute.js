898167ecf557bb47ec4d3c999584222a06883a68
irfiacre <firaduka@outlook.com>
2019-09-13 01:17:47 +0200

    adding  modified swagger and routes [finishes #168469705]

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -9,10 +9,10 @@ import sessionValidator from '../middleware/sessionValidator';
 
 const route = express.Router();
 
-route.post('/api/v1/auth/signin', signinValidater, signing.signin);
-route.post('/api/v1/auth/signup', signupValidater, signing.signup);
-route.get('/api/v1/mentors', Authorisation, allMentors.mentorsDisplay);
-route.get('/api/v1/mentors/:mentorId', Authorisation, allMentors.specificMentorDisplay);
-route.post('/api/v1/sessions', [Authorisation, sessionValidator], session.sessionCreate);
+route.post('/api/v2/auth/signin', signinValidater, signing.signin);
+route.post('/api/v2/auth/signup', signupValidater, signing.signup);
+route.get('/api/v2/mentors', Authorisation, allMentors.mentorsDisplay);
+route.get('/api/v2/mentors/:mentorId', Authorisation, allMentors.specificMentorDisplay);
+route.post('/api/v2/sessions', [Authorisation, sessionValidator], session.sessionCreate);
 
 export default route;
