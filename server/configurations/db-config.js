40ff5365e6675e8425df3eb93033a0f729170381
irfiacre <firaduka@outlook.com>
2019-09-12 07:27:27 +0200

    adding  modified files  and deleted some files for code refactoring  [finishes #168360652]

--- server/configurations/db-config.js
+++ server/configurations/db-config.js
@@ -1,15 +1,31 @@
 import { Pool } from 'pg';
-
 import dotenv from 'dotenv';
-
 
 dotenv.config();
 
+let pool;
+if (process.env.NODE_ENV === 'development') {
+  console.log("connected to the database");
+  
+  pool = new Pool({
+    connectionString: process.env.DEV_DATABASE_URL,
+  });
 
-const connectionString = process.env.DATABASE_URL;
+} else if (process.env.NODE_ENV === 'testing') {
+  pool = new Pool({
+    connectionString: process.env.TESTDATABASE_URL,
+  });
 
-const pool = new Pool({
-  connectionString,
-});
+  pool.on('connect', () => {
+  });
+
+} else if (process.env.NODE_ENV === 'production') {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+  pool.on('connect', () => {
+  });
+}
 
 export default pool;
