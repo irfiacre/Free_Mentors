07f9a24adb3aa393d2d775aa15794a5fc7eed010
irfiacre <firaduka@outlook.com>
2019-09-13 05:28:22 +0200

    adding  modified mentor.test.js and mentorCheck.js  [finishes #168471860]

--- server/middleware/mentorCheck.js
+++ server/middleware/mentorCheck.js
@@ -2,11 +2,28 @@
 import pool from '../configurations/db-config';
 
 const checking = async (req, res, next) => {
-
   if (!req.userData.is_mentor) {
     return res.status(403).json({
       status: 403,
       error: 'Forbidden: Only Mentors can perform this operation',
+    });
+  }
+
+  const retrieve1 = `SELECT * FROM sessions WHERE session_id=${req.params.sessionId} AND status='accepted'`;
+  const { rows: [session1] } = await pool.query(retrieve1);
+  if (session1) {
+    return res.status(400).json({
+      status: 400,
+      error: 'session is already accepted',
+    });
+  }
+
+  const retrieve2 = `SELECT * FROM sessions WHERE session_id=${req.params.sessionId} AND status='rejected'`;
+  const { rows: [session2] } = await pool.query(retrieve2);
+  if (session2) {
+    return res.status(400).json({
+      status: 400,
+      error: 'session is already rejected',
     });
   }
 
@@ -19,6 +36,8 @@ const checking = async (req, res, next) => {
       error: 'session not found',
     });
   }
+
+
   next();
 };
 
